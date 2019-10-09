c9e48a45964efd8f3b8af5253e3f0ccf2efd6287
daniel ashley <danielashley@pursuit.org>
2019-10-08 21:14:52 -0400

    Connected frontend to postgres message data using react hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+
 
 function App() {
+  const [messages, Setmessages ] = useState([])
+useEffect(()=>{
+  axios.get('http://localhost:3003/').then((data)=>{
+    console.log(data)
+  })
+  console.log('hello')
+
+
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </div>
   );
 }
